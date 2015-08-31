3e325716bbcb35656d818267cae936504d7c2eeb
Memo <Memo@Memo-PC>
2015-08-31 15:22:36 -0500

    clase del 31 de agosto

--- src/main/java/mx/unam/fciencias/controller/UsuarioController.java
+++ src/main/java/mx/unam/fciencias/controller/UsuarioController.java
@@ -7,7 +7,10 @@ package mx.unam.fciencias.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import mx.unam.fciencias.model.UsuarioModel;
 
@@ -22,11 +25,22 @@ import mx.unam.fciencias.model.UsuarioModel;
 @SessionScoped
 public class UsuarioController {
     
-    private UsuarioModel usuario=new UsuarioModel();
-    private List<UsuarioModel> usuarios=new ArrayList<>();
+    private UsuarioModel usuario;
+    private List<UsuarioModel> usuarios;
+    private String cadena;
     
-    private String cadena="Hola Mundo... <String>";
+    @PostConstruct
+    public void init(){
+        usuario=new UsuarioModel();
+        usuarios=new ArrayList<>();
+        cadena="Hola Mundo... <String>";
+        System.out.println("Objecto creado");
+    }
     
+    @PreDestroy
+    public void destroy(){
+        System.out.println("Objecto Destruido");
+    }
     
     public void guardausuario(){
         usuarios.add(usuario);
